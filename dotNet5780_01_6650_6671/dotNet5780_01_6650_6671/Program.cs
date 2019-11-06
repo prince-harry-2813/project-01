5a0a8e0ba90ae7a79f48b57fca4a1f30722a6107
yossefsteinmetz1 <57216846+yossefsteinmetz1@users.noreply.github.com>
2019-11-06 20:36:39 +0200

    changes in location
    i ad some problems we should next tme probebly tomorrow set te again the working area

--- dotNet5780_01_6650_6671/dotNet5780_01_6650_6671/Program.cs
+++ dotNet5780_01_6650_6671/dotNet5780_01_6650_6671/Program.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace dotNet5780_01_6650_6671
+namespace part2
 {
     class Program
     {
+        static class RandomNumbers
+        {
+           //
+        }
+        
         static void Main(string[] args)
         {
+            Random r = new Random(DateTime.Now.Millisecond);
+            Int32 [] arrayA = new Int32 [20];
+            Int32 [] arrayB = new Int32 [20];
+            Int32 [] arrayC = new Int32 [20];
+
+            for(int i = 0; i < 20 ; i++)
+            {
+                arrayA[i] = r.Next(18, 123);
+                arrayB[i] = r.Next(18, 123);
+                arrayC[i] = arrayA[i] - arrayB[i];
+                if (arrayC[i] < 0)
+                {
+                    arrayC[i] = arrayC[i] * -1;
+                }
+
+                
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0 , -5 }", arrayA[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0 , -5 }", arrayB[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("{0,-5}", arrayC[i]  );
+            }
+
+            Console.WriteLine();
         }
     }
 }
